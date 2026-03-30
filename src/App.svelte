5927c2d5be3add3556f4bcfb214c4d88f2904d58
Juan Sierra <sierra.juan99@gmail.com>
2026-03-30 16:04:45 -0700

    feat: get moon data

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,38 @@
+<script>
+  import { getMoonTimes, getMoonIllumination } from "suncalc";
+  import { onMount } from "svelte";
 
+  class Moon {
+    dailyData = $state([]);
+    todaysDate = new Date();
+
+    constructor(coordinates) {
+      this.coordinates = coordinates ?? {
+        longitude: 33.9908,
+        latitude: -116.0592,
+      };
+    }
+
+    getMoonData(nDays) {
+      const data = [];
+      for (let i = 0; i < nDays; i++) {
+        const currentDate = new Date(this.todaysDate);
+        currentDate.setDate(currentDate.getDate() + i);
+        const moonTimes = getMoonTimes(
+          currentDate,
+          this.coordinates.longitude,
+          this.coordinates.latitude,
+        );
+        const moonIllumination = getMoonIllumination(currentDate);
+        data.push({ ...moonIllumination, ...moonTimes, currentDate });
+      }
+      this.dailyData = data;
+    }
+  }
+
+  const moon = new Moon();
+
+  onMount(() => {
+    moon.getMoonData(15);
+  });
+</script>
